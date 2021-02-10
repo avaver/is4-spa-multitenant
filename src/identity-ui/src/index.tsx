2314e731a5f084b21e8d7edf6cab8f0e154bc292
avaver <avaver@gmail.com>
2021-02-10 11:18:01 +0200

    wtf

--- src/identity-ui/src/index.tsx
+++ src/identity-ui/src/index.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// import './index.css';
-// import './App.css';
-// import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './Login';
 import Logout from './Logout';
@@ -26,8 +23,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
